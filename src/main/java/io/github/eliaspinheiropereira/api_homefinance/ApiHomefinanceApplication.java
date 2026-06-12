00ed54184f4d7c291f33fb0ba047187f56aaa748
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-06-12 17:12:41 -0300

    iniciando a estrutura do projeto

---
+++ src/main/java/io/github/eliaspinheiropereira/api_homefinance/ApiHomefinanceApplication.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.api_homefinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHomefinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHomefinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/eliaspinheiropereira/api_homefinance/ApiHomefinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.api_homefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHomefinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
